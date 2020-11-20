10a43e880fbcaadacd08e708547fe6563e776051
iknowahra <iknowahrajoh@gmail.com>
2020-11-20 14:26:47 +0700

    add header

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,29 +4,34 @@ import { useReactiveVar } from '@apollo/client';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 import styled, { ThemeProvider } from 'styled-components';
+import { HashRouter as Router } from 'react-router-dom';
 import GlobalStyles from '../Styles/GlobalStyles';
 
 import Theme from '../Styles/Theme';
-import Router from './Router';
+import Routes from './Router';
 import Footer from './Footer';
+import Header from './Header';
 import { isLogginVar } from '../Apollo/LocalState';
 
 function App() {
   const isLoggedIn = useReactiveVar(isLogginVar);
   const Wrapper = styled.div`
     margin: 0 auto;
-    max-width: 935px;
+    max-width: ${(props) => props.theme.maxWidth};
     width: 100%;
   `;
   return (
     <>
       <ThemeProvider theme={Theme}>
-        <Wrapper>
-          <GlobalStyles />
-          <Router isLoggedIn={isLoggedIn} />
-          <Footer />
-          <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
-        </Wrapper>
+        <GlobalStyles />
+        <Router>
+          <Wrapper>
+            <Header isLoggedIn={isLoggedIn} />
+            <Routes isLoggedIn={isLoggedIn} />
+            <Footer />
+          </Wrapper>
+        </Router>
+        <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
       </ThemeProvider>
     </>
   );
