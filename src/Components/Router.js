55ed89dcc8c52059d71b85b6b3bfed5a5728ceaa
iknowahra <iknowahrajoh@gmail.com>
2020-11-22 19:15:27 +0700

    search component

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,17 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Auth from '../Routes/Auth/index';
+import Search from '../Routes/Search/index';
 import Feed from '../Routes/Feed';
 import Explore from '../Routes/Explore';
-import Search from '../Routes/Search';
 import Profile from '../Routes/Profile';
 
 const LoggedInRoutes = () => (
   <Switch>
     <Route exact path="/" component={Feed} />
-    <Route exact path="/explore" component={Explore} />
-    <Route exact path="/search" component={Search} />
-    <Route exact path="/profile/:username" component={Profile} />
+    <Route path="/explore" component={Explore} />
+    <Route path="/search" component={Search} />
+    <Route path="/profile/:username" component={Profile} />
   </Switch>
 );
 
