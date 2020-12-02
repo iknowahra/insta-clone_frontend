8200fc74b3dde175854552a2afc3045dadaee774
iknowahra <iknowahrajoh@gmail.com>
2020-12-02 23:32:37 +0700

    edit router

--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -1,1 +1,8 @@
-export default () => 'Profile';
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import FollowButton from '../Components/FollowButton';
+
+export default withRouter(({ location: { pathname } }) => {
+  const userName = pathname.split('/profile/')[1];
+  return <FollowButton id={8} amIFollowing={false} />;
+});
